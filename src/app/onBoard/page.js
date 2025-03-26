2e015d3254a6cee866a55b4ef5cff690cb83841a
dsjramesh7 <rameshdussa07@gmail.com>
2025-03-26 16:04:44 +0530

    practice

--- src/app/onBoard/page.js
+++ src/app/onBoard/page.js
@@ -1,7 +1,22 @@
 import OnBoard from "@/components/on-board";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-const OnBoardPage = () => {
-  return <OnBoard />;
+const OnBoardPage = async () => {
+  // get the user from auth clerk
+  const user = await currentUser();
+
+  // fetch the profile info => either the user is candidate/ the user is recruiter
+  const profileInfo = await fetchUserAction(user?.id);
+  if (profileInfo?._id) {
+    if (profileInfo?.role === "recruiter" && !profileInfo.isPremiumUser) {
+      redirect("/membership");
+    } else {
+      redirect("/");
+    }
+  } else {
+    return <OnBoard />;
+  }
 };
 
 export default OnBoardPage;
