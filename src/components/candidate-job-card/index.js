6c94874a1696296cfc191bfa49decacec303a290
dsjramesh7 <rameshdussa07@gmail.com>
2025-03-28 19:24:36 +0530

    practice

--- src/components/candidate-job-card/index.js
+++ src/components/candidate-job-card/index.js
@@ -1,21 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import CommonCard from "../common-card";
 import JobIcon from "../job-icon";
 import { Button } from "../ui/button";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "../ui/drawer";
 
 const CandidateJobCard = ({ jobItem, profileInfo }) => {
+  const [showJobDetailsDrawer, setShowJobDetailsDrawer] = useState(false);
   return (
     <>
-      <CommonCard
-        icon={<JobIcon />}
-        title={jobItem?.title}
-        description={jobItem?.companyName}
-        footerContent={
-          <Button className="flex h-11 justify-center items-center px-5 mt-4">
-            View Details
-          </Button>
-        }
-      />
+      <Drawer
+        open={showJobDetailsDrawer}
+        onOpenChange={setShowJobDetailsDrawer}
+      >
+        <CommonCard
+          icon={<JobIcon />}
+          title={jobItem?.title}
+          description={jobItem?.companyName}
+          footerContent={
+            <Button
+              onClick={() => setShowJobDetailsDrawer(true)}
+              className="flex h-11 justify-center items-center px-5 mt-4"
+            >
+              View Details
+            </Button>
+          }
+        />
+        <DrawerContent className="p-6">
+          <DrawerHeader className="px-0">
+            <div className="flex justify-between">
+              <DrawerTitle className="text-4xl font-extrabold text-gray-800">
+                {jobItem?.title}
+              </DrawerTitle>
+              <div className="flex gap-3">
+                <Button className="flex h-11 justify-center items-center px-5 mt-4">
+                  Apply
+                </Button>
+                <Button
+                  onClick={() => setShowJobDetailsDrawer(false)}
+                  className="flex h-11 justify-center items-center px-5 mt-4"
+                >
+                  Cancel
+                </Button>
+              </div>
+            </div>
+          </DrawerHeader>
+          <DrawerDescription className="text-2xl dark:text-white  font-medium text-gray-600">
+            {jobItem?.description}
+            <span className="text-xl dark:text-white  ml-4 font-normal text-gray-500">
+              {jobItem?.location}
+            </span>
+          </DrawerDescription>
+          <div className="w-[150px] mt-6 flex justify-center dark:bg-white  items-center h-[40px] bg-black rounded-[4px]">
+            <h2 className="text-xl font-bold dark:text-black  text-white">
+              {jobItem?.type} Time
+            </h2>
+          </div>
+          <h3 className="text-2xl font-medium text-black mt-3">
+            Experience: {jobItem?.experience} year
+          </h3>
+        </DrawerContent>
+      </Drawer>
     </>
   );
 };
